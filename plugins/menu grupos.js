ec2c3ed6dc83dd767ada10e218497189cca0ecd6
Agromossp <105194926+Agromossp@users.noreply.github.com>
2022-05-09 03:25:52 -0400

    Update menu grupos.js

--- plugins/menu grupos.js
+++ plugins/menu grupos.js
@@ -10,8 +10,8 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let username = conn.getName(who)
 let vn = './media/menu-bienvenidos.mp3'
 let menu =`
-╭━〘 💚⚡️💚⚡️💚⚡️💚⚡️💚 〙━╮
- ‖    ⚡ _*AGROMOS BOT SP_* ⚡
+╭━〘🎭🎭🎭🎭🎭🎭🎭🎭🎭🎭🎭〙━╮
+ ‖    ⚡ _*PARAGUA BOT_* ⚡
  ‖ 💚*¡𝗛ola! ${username}* 💚
  ‖
  ‖ Ⓜ️ *_MENÚ GRUPOS_* Ⓜ️
@@ -46,7 +46,7 @@ let menu =`
 ┗━━━━━━━━━━━━━┛
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
+conn.send3ButtonImg(m.chat, pp, menu, 'PARAGUA-BOT', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'menu-bienvenidos.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
