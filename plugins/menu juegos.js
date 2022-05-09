b9595eadf93e0e3f6504b82243ffc90d496fcceb
Agromossp <105194926+Agromossp@users.noreply.github.com>
2022-05-09 03:28:33 -0400

    Update menu juegos.js

--- plugins/menu juegos.js
+++ plugins/menu juegos.js
@@ -10,8 +10,8 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let username = conn.getName(who)
 let vn = './media/juegos-bot.mp3'
 let menu =`
-╭━〘 💚⚡️💚⚡️💚⚡️💚⚡️💚 〙━╮
- ‖    ⚡ _*AGROMOS BOT SP_* ⚡
+╭━〘 🎭🎭🎭🎭🎭🎭🎭🎭🎭🎭〙━╮
+ ‖    ⚡ _*PARAGUA BOT _* ⚡
  ‖ 💚*¡𝗛ola! ${username}* 💚
  ‖
  ‖ Ⓜ️ *_MENÚ JUEGOS* Ⓜ️
@@ -45,7 +45,7 @@ let menu =`
 ┗━━━━━━━━━━━━━┛
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', 'HOLA', `Hola`, '𝙼𝙴𝙽𝚄 INFORMACIÓN ', `#minformacion`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
+conn.send3ButtonImg(m.chat, pp, menu, 'PARAGUA - BOT', 'HOLA', `Hola`, '𝙼𝙴𝙽𝚄 INFORMACIÓN ', `#minformacion`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'juegos-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
