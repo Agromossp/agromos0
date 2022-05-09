c9dbe7c0dc8b9c415080dd036460bfb709c09ce5
Agromossp <105194926+Agromossp@users.noreply.github.com>
2022-05-09 10:18:27 -0400

    Update menu atajos.js

--- plugins/menu atajos.js
+++ plugins/menu atajos.js
@@ -10,8 +10,8 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let username = conn.getName(who)
 let vn = './media/atajos-bot.mp3'
 let menu =`
-╭━〘 💚⚡️💚⚡️💚⚡️💚⚡️💚 〙━╮
- ‖    ⚡ _*AGROMOS BOT SP_* ⚡
+╭━〘 🎭🎭🎭🎭🎭🎭🎭🎭 〙━╮
+ ‖    ⚡ _*PARAGUA BOT_* ⚡
  ‖ 💚*¡𝗛ola! ${username}* 💚
  ‖
   ‖ Ⓜ️ *_MENÚ ATAJOS_* Ⓜ️
@@ -47,7 +47,7 @@ let menu =`
 ┗━━━━━━━━━━━━━┛
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 CREADOR', `#menucreador`, '𝙼ENU GRUPOS', `#menugrupos`, 'MENU DESCARGAS', `#menudescargas`, m, false, { contextInfo: { mentionedJid }})   
+conn.send3ButtonImg(m.chat, pp, menu, 'PARAGUA BOT', '𝙼𝙴𝙽𝚄 CREADOR', `#menucreador`, '𝙼ENU GRUPOS', `#menugrupos`, 'MENU DESCARGAS', `#menudescargas`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'atajos-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
